341f93f3d679a02372d7491d21cadb5a2d6a87b7
unknown <govetrikayque@gmail.com>
2022-09-02 14:14:12 -0300

    fix: remove files and node_modules

--- dist/botcity/maestro/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const ensureAccessToken: (_target: Object, _propertyKey: string, descriptor: PropertyDescriptor) => PropertyDescriptor;
-export declare const catchError: (_target: Object, _propertyKey: string, descriptor: PropertyDescriptor) => void;
